df41d772ae883388aac905f1568ec75d85cc1b13
Allyson <allysondev.tech@outlook.com>
2023-06-03 14:28:26 -0300

    Página em homenagem ao meu amor.

---
+++ pages/index.js
@@ -0,0 +1,17 @@
+function Home(){
+    let mystyle = {
+       display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        justifyContent: "center",
+        width: "100vw"
+    };
+    return <div style={mystyle}>
+        <h1>Evelin</h1>
+        <br/>
+        <h1>Você é um dom maravilhoso de Deus na minha vida. Sou apaixonado pela pessoa que você é. Amo cada detalhe e aspecto do teu ser. Obrigado por entrar na minha vida!</h1>
+        <img src="https://magiagifs.com.br/wp-content/uploads/2019/08/gifs-coracao-pulsando-apaixonado2.gif" alt=""/>
+    </div>
+}
+
+export default Home
